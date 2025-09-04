5e022ff5dd76907565ddaecbf915863215166d9a
ivandenysevych <denysevych.i.v_kn24@rcit.ukr.education>
2025-09-04 19:06:41 +0300

    Initial commit: додав lab1 з класами Figure, Circle, Square

---
+++ lab1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Figure circle = new Circle("Коло", 19);
+        Console.WriteLine($"{circle.Name}: площа = {circle.GetArea()}");
+
+        Figure square = new Square("Квадрат", 10);
+        Console.WriteLine($"{square.Name}: площа = {square.GetArea()}");
+
+        Console.ReadKey();
+    }
+}
+
+abstract class Figure
+{
+    public Figure(string name, float radius)
+    {
+        Name = name;
+        Radius = radius;
+    }
+
+    public string Name { get; set; }
+    public float Radius { get; set; }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
+
+class Circle : Figure
+{
+    public Circle(string name, float radius) : base(name, radius) { }
+
+    public override double GetArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+}
+
+class Square : Figure
+{
+    public Square(string name, float side) : base(name, side) { }
+
+    public override double GetArea()
+    {
+        return Radius * Radius; 
+    }
+}
