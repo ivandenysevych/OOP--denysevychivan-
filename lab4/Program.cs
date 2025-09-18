9a4201fb74273b586dc84cfd1d16229dac52dd8a
ivandenysevych <denysevych.i.v_kn24@rcit.ukr.education>
2025-09-18 18:53:11 +0300

    Lab4: реалізація з коментарями (інтерфейс, абстрактний клас, AreaCalculator)

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -9,26 +9,33 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
+            // Встановлюємо культуру для відображення чисел (щоб була крапка замість коми)
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
+            // Створюємо список фігур, які реалізують інтерфейс IArea
             var shapes = new List<IArea>
             {
-                new Circle(3.5),
-                new Rectangle(4, 7),
-                new Circle(1.2),
-                new Rectangle(10, 2.5)
+                new Circle(3.5),      // Коло з радіусом 3.5
+                new Rectangle(4, 7),  // Прямокутник зі сторонами 4 і 7
+                new Circle(1.2),      // Коло з радіусом 1.2
+                new Rectangle(10, 2.5)// Прямокутник зі сторонами 10 і 2.5
             };
 
+            // Використовуємо клас AreaCalculator (композиція) для роботи зі списком фігур
             var calc = new AreaCalculator(shapes);
 
             Console.WriteLine("-- All shapes --");
+            // Виводимо усі фігури з їхніми параметрами
             foreach (var s in shapes)
                 Console.WriteLine(s);
 
+            // Рахуємо сумарну площу всіх фігур
             double totalArea = calc.TotalArea();
+            // Шукаємо фігури з мінімальною та максимальною площею
             var (minShape, maxShape) = calc.MinMax();
 
             Console.WriteLine();
+            // Вивід результатів у консоль
             Console.WriteLine("Total area: {0:0.###}", totalArea);
             Console.WriteLine("Min area: {0} with S={1:0.###}",  minShape.Name, minShape.Area());
             Console.WriteLine("Max area: {0} with S={1:0.###}",  maxShape.Name, maxShape.Area());
