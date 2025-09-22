ed6bc250af67cccba1e22d7984c274c62406a72f
ivandenysevych <denysevych.i.v_kn24@rcit.ukr.education>
2025-09-22 18:13:46 +0300

    Remove lab4 to reinitialize from scratch

--- lab4/Domain/IArea.cs
+++
@@ -1,9 +0,0 @@
-﻿// Інтерфейс: фігура має імʼя та вміє повертати свою площу.
-namespace lab4.Domain
-{
-    public interface IArea
-    {
-        string Name { get; }
-        double GetArea();
-    }
-}
--- lab4/Models/IArea.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab4.Models
-{
-    // Контракт для фігур: імʼя + метод обчислення площі
-    public interface IArea
-    {
-        string Name { get; }
-        double Area();
-    }
-}
